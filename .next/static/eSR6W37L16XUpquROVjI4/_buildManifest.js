d875689de926ee76e26690e38ec3ebcc977db800
Anasirfan-ix <anasirfan101010@gmail.com>
2025-06-17 18:20:35 +0500

    changes

---
+++ .next/static/eSR6W37L16XUpquROVjI4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/526-90adbc2033a40c6b.js","static/chunks/pages/index-9bd273d3278a9f7f.js"],"/_error":["static/chunks/pages/_error-08a9db0f433628d8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
